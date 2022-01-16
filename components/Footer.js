c6939939eaa459c1dd069aa70c0a343be81ddb86
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-16 11:16:00 -0700

    remove excess from Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,37 +1,11 @@
-import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
-import { MdOutlineMail } from 'react-icons/md'
 import { SocialLinks, METADATA } from '../utils/constants'
-
-import utilStyles from '../styles/utils.module.css'
-
-/**
- * TODO:
- *  - no longer sticky, just display at the bottom of every page
- *  - move social links to bottom of full-screen navigation
- */
 
 const Footer = () => (
   <footer>
-    <small className={utilStyles.copyright}>
-      © {new Date().getFullYear()}{' '}
-      <a href={SocialLinks.PersonalHomepage} target="_blank">
-        {METADATA.NAME}
-      </a>
-    </small>
-    <div className={utilStyles.socialIcons}>
-      <a href={SocialLinks.Email} network="email" target="_blank">
-        <MdOutlineMail />
-      </a>
-      <a href={SocialLinks.Twitter} target="_blank">
-        <FaTwitter />
-      </a>
-      <a href={SocialLinks.LinkedIn} target="_blank">
-        <FaLinkedinIn />
-      </a>
-      <a href={SocialLinks.Github} target="_blank">
-        <FaGithub />
-      </a>
-    </div>
+    © {new Date().getFullYear()}{' '}
+    <a href={SocialLinks.PersonalHomepage} target="_blank">
+      {METADATA.NAME}
+    </a>
   </footer>
 )
 
