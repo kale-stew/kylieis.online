555fdd5ab8dba68102f9fb410669e35dd220c74a
Kylie Stewart <kylie@kyliestewart.tech>
2022-01-25 20:40:32 -0700

    Feature: update MarkdownHighlight theme

--- components/MarkdownHighlight.js
+++ components/MarkdownHighlight.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { synthwave84 } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 const MarkdownHighlight = ({ language, value }) => (
-  <SyntaxHighlighter language={language} style={synthwave84}>
+  <SyntaxHighlighter language={language} style={materialDark}>
     {value}
   </SyntaxHighlighter>
 )
